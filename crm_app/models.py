bc1af1534d6b05d288debc3b06a0d14d6da3f877
Artur Zacniewski <a.zacniewski@gmail.com>
2016-04-03 14:30:43 +0200

    Main models and added app to INSTALLED_APPS

--- crm_app/models.py
+++ crm_app/models.py
@@ -2,11 +2,34 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-"""
+
 class CrmUser(models.Model):
     user = models.OneToOneField(User)
 
+    def __str__(self):
+        return 'Profile of user: {}'.format(self.user.username)
+
 
 class Company(models.Model):
-    name = models.CharField()
-"""
+    SECTOR_CHOICES = (
+        ('it', 'IT'),
+        ('finance', 'Finance'),
+        ('education', 'Education')
+    )
+    NUMBER_OF_EMPLOYEES_CHOICES = (
+        ('small', '1-20'),
+        ('medium', '21-50'),
+        ('large', '51-100'),
+        ('xxl', '>100')
+    )
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=50)
+    sector = models.CharField(max_length=10,
+                              choices=SECTOR_CHOICES,
+                              default='it')
+    number_of_employees = models.CharField(max_length=10,
+                                           choices=NUMBER_OF_EMPLOYEES_CHOICES,
+                                           default='medium')
+
+    def __str__(self):
+        return self.name
