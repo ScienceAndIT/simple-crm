16e3348715c490baa893cdd8f1a843b4e7ce0c11
Artur Zacniewski <a.zacniewski@gmail.com>
2016-04-04 14:20:46 +0200

    New field in models

--- crm_app/models.py
+++ crm_app/models.py
@@ -6,31 +6,33 @@ from django.contrib.auth.models import User
 class CrmUser(models.Model):
     user = models.OneToOneField(User)
     # new fields can be added here to extend this model
+    added_by = models.CharField(max_length=50, default='')
 
     def __str__(self):
-        return 'Profile of user: {}'.format(self.user.username)
+        return self.user.username
 
 
 class Company(models.Model):
     SECTOR_CHOICES = (
-        ('it', 'IT'),
-        ('finance', 'Finance'),
-        ('education', 'Education')
+        ('IT', 'IT'),
+        ('Finance', 'Finance'),
+        ('Education', 'Education')
     )
     NUMBER_OF_EMPLOYEES_CHOICES = (
-        ('small', '1-20'),
-        ('medium', '21-50'),
-        ('large', '51-100'),
-        ('xxl', '>100')
+        ('1-20', '1-20'),
+        ('21-50', '21-50'),
+        ('51-100', '51-100'),
+        ('>100', '>100')
     )
-    name = models.CharField(max_length=100)
-    owner = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, blank=False)
+    owner = models.CharField(max_length=50, blank=False)
     sector = models.CharField(max_length=10,
                               choices=SECTOR_CHOICES,
                               default='it')
     number_of_employees = models.CharField(max_length=10,
                                            choices=NUMBER_OF_EMPLOYEES_CHOICES,
                                            default='medium')
+    added_by = models.CharField(max_length=50, default='')
 
     def __str__(self):
         return self.name
