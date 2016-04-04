e9bb2708603bb20aae7d756a36418efd147c71ff
Artur Zacniewski <a.zacniewski@gmail.com>
2016-04-04 17:20:09 +0200

    Changed name of the view

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     #url(r'^zaloguj/$', views.user_login, name='login'),
-    url(r'login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'companies/$', views.companies_view, name='companies'),
-    url(r'add-company/$', views.companies_view, name='companies'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^companies/$', views.companies_view, name='companies'),
+    url(r'^add-company/$', views.add_company_view, name='add_company'),
+    url(r'^users/$', views.users_view, name='users_view'),
+    url(r'^add-user/$', views.add_user_view, name='add_user'),
     url(r'^password-change/$',
         'django.contrib.auth.views.password_change',
         name='password_change'),
     url(r'^done-password-change/$',
         'django.contrib.auth.views.password_change_done',
-        name='password_change_done'),
-    url(r'^$', views.companies_view, name='companies_view'),
+        name='done_password_change'),
     ]
