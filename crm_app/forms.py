fc3fc161d22074c9742783d21bd03fb9199d3c69
Artur Zacniewski <a.zacniewski@gmail.com>
2016-04-04 14:20:15 +0200

    Added field for info who added company

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -1,16 +1,17 @@
 from django import forms
-from .models import CrmUser, Company
+from django.contrib.auth.models import User
+from .models import Company
 
 
 class CrmUserForm(forms.ModelForm):
 
     class Meta:
-        model = CrmUser
-        fields = ('username', 'firstname', 'lastname', 'password')
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password')
 
 
 class CompanyForm(forms.ModelForm):
 
     class Meta:
         model = Company
-        fields = ('name', 'owner', 'sector', 'number_of_employees')
+        fields = ('name', 'owner', 'sector', 'number_of_employees', 'added_by')
