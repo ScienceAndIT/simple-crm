7da35df3ce9ddd60b6eae95eb76c4951ce8a78b2
Artur Zacniewski <a.zacniewski@gmail.com>
2016-04-06 10:11:14 +0200

    Changing password for user is now possible

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^zaloguj/$', views.user_login, name='login'),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^companies/$', views.companies_view, name='companies'),
@@ -12,7 +11,7 @@ urlpatterns = [
     url(r'^password-change/$',
         'django.contrib.auth.views.password_change',
         name='password_change'),
-    url(r'^done-password-change/$',
+    url(r'^password-change/done/$',
         'django.contrib.auth.views.password_change_done',
-        name='done_password_change'),
-    ]
+        name='password_change_done'),
+]
